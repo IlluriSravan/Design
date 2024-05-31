308ca971cfdfdcd39682fa848c915d12dcc4c018
IlluriSravan <sravankumar6546@gmail.com>
2024-05-31 14:22:42 +0530

    fourth

--- src/components/Box/index.jsx
+++ src/components/Box/index.jsx
@@ -12,7 +12,7 @@ const Box = () => {
         <p>100 years have passed since our founders created what is now the Novo Nordisk Foundation. We are sure they would find today’s world a much better place than the one they lived in.</p>
 
         <p>As we mark our centennial, the Novo Nordisk Foundation reaffirms its commitment to being a catalyst for better human and planetary health. We are mindful of the responsibilities that come with being a resourceful organization. We pledge to be transparent about our work, involve our stakeholders in shaping our strategies, and address the concerns that some may have with the science and technologies that we support. Only this way can we earn the trust of our stakeholders and make a meaningful impact, together.</p>
-        <button type="button">Read More</button>
+        <button className="button-nav" type="button">Read More</button>
     </div >
     <div className="box2">
     <img src="https://novonordiskfonden.dk//app/uploads/novo-nordisk-holdings_01208_high-2-aspect-ratio-720-720-1.png"/>
